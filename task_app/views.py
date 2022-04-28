0d47afb4f6efb0ead313ce4f9f13f209cc078548
Rahmet97 <ruslanovrahmet@gmail.com>
2022-04-29 01:17:38 +0500

    Delete note done

--- task_app/views.py
+++ task_app/views.py
@@ -2,6 +2,8 @@ from .serializer import TaskSerializer, NoteSerializer
 from rest_framework import viewsets, generics, response, status
 from rest_framework.permissions import BasePermission, IsAuthenticated
 from task_app.models import Task, Note
+from rest_framework.views import APIView
+from django.http import Http404
 
 
 class NotePermission(BasePermission):
@@ -44,9 +46,16 @@ class NoteView(generics.ListCreateAPIView):
         serializer.save(author=self.request.user)
 
 
-class NoteDestroyAPIView(generics.DestroyAPIView):
-    queryset = Note.objects.all()
+class NoteDestroyAPIView(APIView):
     permission_classes = [IsAuthenticated, NotePermission]
 
-    def perform_create(self, serializer):
-        serializer.save(author=self.request.user)
+    def get_object(self, pk):
+        try:
+            return Note.objects.get(pk=pk)
+        except Note.DoesNotExist:
+            raise Http404
+
+    def delete(self, request, pk):
+        note = self.get_object(pk)
+        note.delete()
+        return response.Response(status=status.HTTP_204_NO_CONTENT)
