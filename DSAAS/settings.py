5d65013361ea640ecb05ec0d05b68c3dda0ea1e1
Rahmet97 <ruslanovrahmet@gmail.com>
2022-05-21 14:33:31 +0500

    Changes

--- DSAAS/settings.py
+++ DSAAS/settings.py
@@ -220,7 +220,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Tashkent'
 
 USE_I18N = True
 
@@ -254,3 +254,6 @@ CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_RESULT_SERIALIZER = 'json'
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_BEAT_SCHEDULER = 'django_celery_beat.schedulers:DatabaseScheduler'
+
+BOT_TOKEN = "5269482912:AAHYYrQ5nR_yrKp9ay8PAfulTatXSuGCh6A"
+
--- telegram/urls.py
+++ telegram/urls.py
@@ -1,7 +1,9 @@
 from django.urls import path
 
-from telegram.views import get_chat_subscribers_count
+from telegram.views import get_chat_subscribers_count, add_channel_or_group, add_post
 
 urlpatterns = [
-    path('get-chat-subscribers-count', get_chat_subscribers_count)
+    path('get-chat-subscribers-count', get_chat_subscribers_count),
+    path('add-channel-or-group', add_channel_or_group),
+    path('add-post', add_post)
 ]
