528b1d29eb9c2fd0f3053d8cfabff284268b8145
Rahmet97 <ruslanovrahmet@gmail.com>
2022-10-10 03:18:19 +0500

    Static files

--- DSAAS/settings.py
+++ DSAAS/settings.py
@@ -41,7 +41,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'drf_yasg',  # for swagger and redoc
-
+    'whitenoise.runserver_nostatic',
     'django.contrib.sites',
 
 ]
