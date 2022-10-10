67aa8220b489a0ca78f7fdf8ec24596f3ec9ef2a
Rahmet97 <ruslanovrahmet@gmail.com>
2022-10-11 00:33:53 +0500

    Static files

--- DSAAS/settings.py
+++ DSAAS/settings.py
@@ -136,7 +136,7 @@ SWAGGER_SETTINGS = {
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # WhiteNoise Middleware
+    # 'whitenoise.middleware.WhiteNoiseMiddleware',  # WhiteNoise Middleware
     'django.contrib.sessions.middleware.SessionMiddleware',
     "corsheaders.middleware.CorsMiddleware",  # middleware for django-cors-headers
     'django.middleware.common.CommonMiddleware',
@@ -237,7 +237,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
+# STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
