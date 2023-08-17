4c64611a343e1b3091b046119ad9e99daddb921d
Rahmet97 <54974456+Rahmet97@users.noreply.github.com>
2023-08-18 03:15:59 +0500

    Update settings.py

--- DSAAS/settings.py
+++ DSAAS/settings.py
@@ -193,14 +193,14 @@ WSGI_APPLICATION = 'DSAAS.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'digitalsaas_db',
-        # 'USER': 'dsaas_admin',
-        # 'PASSWORD': 'Digital2022SASS',
-        # 'HOST': 'localhost',
-        # 'PORT': '',
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'digitalsaas_db',
+        'USER': 'dsaas_admin',
+        'PASSWORD': 'Digital2022SASS',
+        'HOST': 'digitalsaas-db.chvtezjdjpew.eu-north-1.rds.amazonaws.com',
+        'PORT': 5432,
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
